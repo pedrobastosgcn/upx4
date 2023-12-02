39400e1c9467c009c2652f03eecd7aa7d1b0a707
jhunhoff <joao.hunhoff2@gmail.com>
2023-12-02 18:42:29 -0300

    fixed error in the sql script, centralized text in the catalog table

--- ecoCatalog/database/script.sql
+++ ecoCatalog/database/script.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS catalog_table (
     id SERIAL PRIMARY KEY,
-    material VARCHAR(100) NOT NULL    
-    decomposition_time INT NOT NULL
+    material VARCHAR(100) NOT NULL,    
+    decomposition_time INT NOT NULL,
     time_unit VARCHAR(10) NOT NULL
 );
